f2a70e86570677cfd131d38d40bbbc05d4fe4a57
AliGorithm <alisher.coder@gmail.com>
2018-04-02 21:48:15 -0500

    Designing in Playground2

--- DesingingInPlayground.xcplaygroundpage/Contents.swift
+++ DesingingInPlayground.xcplaygroundpage/Contents.swift
@@ -13,14 +13,25 @@ class MyPlaygroundView: UIViewController {
     
     override func loadView() {  // This method loads or creates a view and assigns it to the view property.
         let view = UIView()
-        view.backgroundColor = .white
+        view.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
         
+        
+        // Add CardTextView
+        let textCardView = UIView()
+        textCardView.frame = CGRect(x: 20, y: 255, width: 300, height: 250)
+        textCardView.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        textCardView.layer.cornerRadius = 20.0
+        textCardView.layer.shadowOpacity = 10.0
+        textCardView.layer.shadowOffset = CGSize(width: 0, height: 5)
+        textCardView.layer.shadowRadius = 15.0
         let lable = UILabel()
-        lable.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        lable.frame = CGRect(x: 16, y: 16, width: textCardView.frame.width - 20, height: 20)
         lable.text = "Hello World This is AliGorithm"
         lable.textColor = .black
+        lable.numberOfLines = 0
         
-        view.addSubview(lable)
+        view.addSubview(textCardView)
+        textCardView.addSubview(lable)
         self.view = view
     }
     
@@ -28,4 +39,5 @@ class MyPlaygroundView: UIViewController {
 
 //// TO Present the ViewController in the Live View Window
 PlaygroundPage.current.liveView = MyPlaygroundView()
+
 //: [Next](@next)
